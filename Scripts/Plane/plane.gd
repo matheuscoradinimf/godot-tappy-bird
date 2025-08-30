1dbf6394d7af7f8193cbb48e70d19997fa3d104a
Matheus <coradinimatheus1@gmail.com>
2025-08-30 20:54:46 -0300

    plane jump and fly

--- Scripts/Plane/plane.gd
+++ Scripts/Plane/plane.gd
@@ -1,11 +1,27 @@
 extends CharacterBody2D
 
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const JUMP_FORCE: float = 300
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func die() -> void:
+	animated_sprite.stop()
+	set_physics_process(false)
+	
+func fly(delta: float) -> void:
+	velocity.y += _gravity * delta
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -JUMP_FORCE
+		animation_player.play("jump")
+	
+	
+
+func _physics_process(delta: float) -> void:
+	fly(delta)
+	move_and_slide()
+	if is_on_floor():
+		die()
+		
